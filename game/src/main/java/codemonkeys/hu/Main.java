cf37c6fd3170997b6a5a5b633d6c82af9fcd8d4b
Oláh Norbert <pst8ra@inf.elte.hu>
2023-03-31 18:50:21 +0000

    Revert "Merge branch 'gui' into 'dev'"
    This reverts merge request !2

--- game/src/main/java/codemonkeys/hu/Main.java
+++ game/src/main/java/codemonkeys/hu/Main.java
@@ -3,7 +3,5 @@ package codemonkeys.hu;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("XD");
-        System.out.println("XD");
     }
 }
